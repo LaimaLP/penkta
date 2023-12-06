67188f4c3ef18865986a802a298ece1b5ae245e8
LaimaLP <pileliene.laima@gmail.com>
2023-12-06 18:33:08 +0200

    BookShop

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.scss";
+import { BooksProvider } from "./components/BooksContext";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <BooksProvider>
+      <div className="app">
+        <div className="bin">
+          <Layout />
+        </div>
+      </div>
+    </BooksProvider>
+  );
+}
+
+export default App;
