993c3d9ccdd6fb3a7abd8f0789f2c6c168e5f754
LaimaLP <pileliene.laima@gmail.com>
2023-12-08 16:04:21 +0200

    sort uodate

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,52 +1,54 @@
-import { useContext, useEffect, useState } from "react";
-import { BooksContext } from "./BooksContext";
+import { useState, useContext, useEffect } from 'react';
+import { BooksContext } from './BooksContext';
 
 const sorts = [
-  { title: "Nerūšiuota", id: 0 },
-  { title: "Pavadinimas ↑", id: 1 },
-  { title: "Pavadinimas ↓", id: 2 },
-  { title: "Kaina ↑", id: 3 },
-  { title: "Kaina ↓", id: 4 },
+    { title: 'Nerūšiuota', id: 0 },
+    { title: 'Pavadinimas ↑', id: 1 },
+    { title: 'Pavadinimas ↓', id: 2 },
+    { title: 'Kaina ↑', id: 3 },
+    { title: 'Kaina ↓', id: 4 },
 ];
 
 export default function Sort() {
-  const [sort, setSort] = useState(0);
-  const {setBooks, books} = useContext(BooksContext);
 
-  useEffect(_ => {
-    if(books === null)
-switch(sort){
-    case 1:
-        setBooks(prev => [...prev].sort((a, b) => a.title.localeCompare(b.title)));
-        break;
-    case 2:
-        setBooks(prev => [...prev].sort((a, b) => b.title.localeCompare(a.title)));
-        break;
-    case 3:
-        setBooks(prev => [...prev].sort((a, b) => a.price - b.price));
-        break;
-    case 4:
-        setBooks(prev => [...prev].sort((a, b) => b.price - a.price));
-        break;
-    default:
+    const [sort, setSort] = useState(0);
 
-}
-  }, [sort]);
+    const { setBooks, books } = useContext(BooksContext);
 
+    useEffect(_ => {
 
+        if (books === null) {
+            return;
+        };
 
-  return (
-    <>
-      <div className="sort">
-      {
+        switch (sort) { //kai sortinam, butina tai daryti ant kopijos, nes sortas nedaro kopijas ir jei daraysim ant orginalo, niekas nesikeis
+            case 1:
+                setBooks(prev => [...prev].sort((a, b) => a.title.localeCompare(b.title)));
+                break;
+            case 2:
+                setBooks(prev => [...prev].sort((a, b) => b.title.localeCompare(a.title)));
+                break;
+            case 3:
+                setBooks(prev => [...prev].sort((a, b) => a.price - b.price));
+                break;
+            case 4:
+                setBooks(prev => [...prev].sort((a, b) => b.price - a.price));
+                break;
+            default:
+        }
+
+    }, [sort]);
+
+    return (
+        <div className="sort">
+            {
                 sorts.map(s => <div key={s.id} className="sort__button" style={
                     {
                         color: sort === s.id ? 'crimson' : null,
                         cursor: sort === s.id ? 'default' : 'pointer'
                     }
-                } onClick={() => setSort(s.id)}>{s.title}</div>)
+                } onClick={_ => setSort(s.id)}>{s.title}</div>)
             }
-      </div>
-    </>
-  );
+        </div>
+    )
 }
